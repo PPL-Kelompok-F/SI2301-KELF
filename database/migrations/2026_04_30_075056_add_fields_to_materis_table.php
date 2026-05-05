78c1a7fd007bdb877dd1027702f0d6fcef475a8b
fakhrinagna <fakhrinza@gmail.com>
2026-05-05 14:14:52 +0700

    Menambahkan Scrum 91, Scrum 93, dan Scrum 89

---
+++ database/migrations/2026_04_30_075056_add_fields_to_materis_table.php
@@ -0,0 +1,31 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::table('materis', function (Blueprint $table) {
+            $table->string('Judul');
+            $table->text("deskripsi");
+            $table->string('tipe')->nullable();
+            $table->string('file')->nullable();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::table('materis', function (Blueprint $table) {
+            $table->dropColumn(['Judul', 'deskripsi','tipe', 'file']);
+        });
+    }
+};
